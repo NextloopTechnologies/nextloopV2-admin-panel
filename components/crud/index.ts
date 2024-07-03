94579ce001ce32911e4b01c2e6dcd2dd65922f2d
sanadkhan-nextloop <skhan@nextlooptechnologies.com>
2024-07-03 12:49:36 +0530

    fixed import error

--- components/crud/index.ts
+++ components/crud/index.ts
@@ -1,11 +1,5 @@
 import SearchBox from "./SearchBox";
-import ActionButton from "./ActionButton";
-import ViewLink from "./ViewLink";
-import ViewModal from "./ViewModal";
 
 export {
-  SearchBox,
-  ActionButton,
-  ViewLink,
-  ViewModal
+  SearchBox
 }
