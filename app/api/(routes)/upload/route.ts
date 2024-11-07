30efd546100c872251f73a51ee93c721d2af8c38
sanadkhan-nextloop <skhan@nextlooptechnologies.com>
2024-11-07 14:52:04 +0530

    fixed cors for upload API

--- app/api/(routes)/upload/route.ts
+++ app/api/(routes)/upload/route.ts
@@ -1,5 +1,18 @@
 import { UploadFileService } from "../..";
 
+export async function OPTIONS() {
+    // Handle OPTIONS request
+    return new Response(null, {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': '*', // Adjust this in production
+            'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+        }
+    });
+}
+
 export async function POST(req: Request) {
     try {
         const formData = await req.formData();
@@ -9,8 +22,10 @@ export async function POST(req: Request) {
         if (imageInfo === null) return Response.json({ msgText: "File is required!" }, { status: 400 })
 
         const { fileId, url } = await UploadFileService.uploadImage(imageInfo, imageInfo.name);
-
-        if (fileId) return Response.json({ success: true, data: { fileId, url }, msgText: "Uploaded!" }, { status: 201 });
+        
+        if (fileId) return Response.json({ success: true, data: { fileId, url }, msgText: "Uploaded!" }, { status: 201, headers: {
+            'Access-Control-Allow-Origin': '*'
+        } });
         return Response.json({ success: false, msgText: "Failed to upload!" }, { status: 500 });
     } catch (error) {
         console.error("FILE_UPLOAD_CONTROLLER", error)
