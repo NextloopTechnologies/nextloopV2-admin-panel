84f7402e502a6a56f66b2206bd55818d49336480
abc <divyabilthare625@gmail.com>
2026-08-31 10:44:24 -0400

    fix: html support

--- components/blog/Form.tsx
+++ components/blog/Form.tsx
@@ -135,16 +135,18 @@ const BlogForm: React.FC<BlogFormProps> = ({
       });
       setIsSlugManuallyEdited(!!blog.slug);
       setIsCanonicalManuallyEdited(!!blog.canonical_url);
-      if (blog.image?.length) {
-        const files = blog.image.map((file: any) => {
-          return {
-            ...file,
-            status: 'done'
-          }
-        });
-        setFileList(files);
-      }
+     if (blog.image && Array.isArray(blog.image) && blog.image.length > 0) {
+      const files = blog.image.map((file: any) => {
+        return {
+          ...file,
+          status: 'done'
+        }
+      });
+      setFileList(files);
+    } else {
+      setFileList([]);
     }
+  }
   }, [blog, form]);
 
   const imageHandler = () => {
